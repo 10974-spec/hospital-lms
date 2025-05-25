d68b1e00d5f5d8e6d41d5def445768886083df11
Emmanuel Nyakoe <emmanuelnyakoe13@gmail.com>
2025-05-25 23:17:29 +0500

    backend folders and files

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+
+// APP CONFIG 
+const app = express();
+const port = process.env.PORT || 4000;
+
+// MIDDLEWARES
+app.use(express.json());
+app.use(cors());
+
+// API ENDPOINTS
+app.get('/', (req, res) => {
+    res.send('API WORKING! ');
+});
+
+app.listen(port, () => {
+    console.log("server started", port);
+});
+
