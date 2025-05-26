52ddcf370141dde6f2cfe58f0cb3b74319cee10d
Emmanuel Nyakoe <emmanuelnyakoe13@gmail.com>
2025-05-26 23:22:37 +0500

    controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './config/mongodb.js';
 import conectCloudinary from './config/cloudinary.js';
+import adminRouter from './routes/adminRoute.js';
 
 
 // APP CONFIG 
@@ -16,6 +17,9 @@ app.use(express.json());
 app.use(cors());
 
 // API ENDPOINTS
+app.use('/api/admin', adminRouter);
+// localhost:4000/api/admin/
+
 app.get('/', (req, res) => {
     res.send('API WORKING! ');
 });
