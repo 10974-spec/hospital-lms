1039ef6adb4c593e3a17db261f082570654d6582
Emmanuel Nyakoe <emmanuelnyakoe13@gmail.com>
2025-05-30 22:35:12 +0300

    API TO UPDATE USER DATA

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
+import {v2 as cloudinary} from 'cloudinary'
 
 // API TO REGISTER USER
 
@@ -70,7 +71,7 @@ const loginUser = async (req, res) => {
     const user = await userModel.findOne({ email });
 
     if (!user) {
-    return res.json({
+      return res.json({
         success: false,
         message: "User does not exist",
       });
@@ -110,16 +111,65 @@ const getProfile = async (req, res) => {
     res.json({
       success: true,
       userData,
-    })
-   
-
-  }catch (error) {
+    });
+  } catch (error) {
     console.log(error);
     res.json({
       success: false,
       message: error.message,
     });
   }
-}
+};
+
+// API TO UPDATE USER PROFILE DATA
+
+const updateProfile = async (req, res) => {
+  try {
+    const { userId, name, phone, address, dob, gender } = req.body;
+    const imageFile = req.file;
+
+    if (!name || !phone || !address || !dob || !gender) {
+      return res.json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    await userModel.findByIdAndUpdate(userId, {
+      name,
+      phone,
+      address: JSON.parse(address),
+      dob,
+      gender,
+    });
+
+    if (imageFile) {
+      //  UPLOAD IMAGE TO CLOUDINARY
+      const imageUpload = await cloudinary.uploader.upload(imageFile.path, {
+        resource_type: "image",
+      })
+      const imageUrl = imageUpload.secure_url;
+
+      await userModel.findByIdAndUpdate(userId, {
+        image: imageUrl,
+      });
+
+
+    }
+
+    res.json({
+      success: true,
+      message: "Profile updated successfully",
+    });
+
+    const userData = await userModel.findById(userId);
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 export { registerUser, loginUser, getProfile };
