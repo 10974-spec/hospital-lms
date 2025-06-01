99f0e5148027e4dd88e471bf0481ddfca508d57c
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-01 19:24:09 +0300

    api endpoint for booking appointment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -245,4 +245,4 @@ const bookAppointment = async (req, res) => {
   }
 }
 
-export { registerUser, loginUser, getProfile, updateProfile };
+export { registerUser, loginUser, getProfile, updateProfile , bookAppointment};
