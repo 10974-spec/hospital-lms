d428b9fefa9edb180efb7bbc94ad77896f6ad1d1
10974-spec <emmanuelnyakoe13@gmail.com>
2025-06-04 21:13:27 +0300

    admin dashboard

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import axios from 'axios'
 import { useEffect } from 'react'
 import { toast } from 'react-toastify'
+import {useNavigate} from 'react-router-dom'
 
 const MyAppointments = () => {
   const { backendUrl, token, getDoctorsData} = useContext(AppContext)
@@ -15,6 +16,8 @@ const MyAppointments = () => {
     const dataArray = slotDate.split('-')
     return dataArray[0] + ' ' + month[Number(dataArray[1]) - 1] +'' + dataArray[2]
   }
+  
+  const navigate = useNavigate()
 
   const getUserAppointments = async () => {
     try {
@@ -55,6 +58,20 @@ const MyAppointments = () => {
   }
 
   const appointmentRazorpay = async (appointmentId) => {
+
+    try {
+      const {data} = await axios.post(backendUrl + '/api/user/payment-razorpay', {
+        appointmentId
+      }, {headers: {token}})
+
+      if (data.success) {
+     console.log(data.order)
+      }
+      
+    } catch (error) {
+      console.log(error)
+      toast.error(error.message)
+    }
     
   }
 
@@ -83,7 +100,7 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-end'>
-              {!item.cancelled &&   <button   onClick={()=>appointmentRazorpay}   className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
+              {!item.cancelled &&   <button   onClick={()=>appointmentRazorpay(item._id)}   className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'>Pay Online</button>}
             
               {!item.cancelled &&     <button onClick={()=>cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300'>Cancel Appointment</button> }
               {
